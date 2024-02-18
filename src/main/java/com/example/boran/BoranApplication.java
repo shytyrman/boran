5731c15253c814670c808021925f749b6882879c
The Adventurer <159638419+shytyrman@users.noreply.github.com>
2024-02-18 12:56:55 +0600

    Add files via upload

---
+++ src/main/java/com/example/boran/BoranApplication.java
@@ -0,0 +1,13 @@
+package com.example.boran;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoranApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoranApplication.class, args);
+    }
+
+}
