b7d24a7f3c940c6c39598d5c2a415ac29d1c166b
Olzhas Alimseitov <olzhas547@gmail.com>
2024-02-19 23:50:48 +0600

    Add Dockerfile, docker-compose

--- src/main/java/com/example/boran/parser/SampleParser.java
+++ src/main/java/com/example/boran/parser/SampleParser.java
@@ -1,8 +1,15 @@
 package com.example.boran.parser;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
 public class SampleParser implements Parser{
 
     public void parse(){
-        System.out.println("parse!");
+        Document doc = Jsoup.connect("https://mig.kz/").get();
+        log(doc.title());
     }
+
 }
